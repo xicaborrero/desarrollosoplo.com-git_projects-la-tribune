2da9ce2927dc6420388c78a312a71e3dba75919b
xicaborrero <xicadepato@gmail.com>
2015-08-09 17:41:21 -0500

    Comenzando a editar el tema
    Estos son cambios principales, de imagenes poco a poco

--- wp-content/themes/sOplo_latribune/js/default_dynamic.js
+++ wp-content/themes/sOplo_latribune/js/default_dynamic.js
@@ -29,6 +29,9 @@ var add_for_admin_bar = 0;
 
 var logo_height = 130; // proya logo height
 var logo_width = 280; // proya logo width
+	logo_height = 104;
+	logo_width = 328;
+	
 	header_top_height = 0;
 var loading_text;
 loading_text = 'Loading new posts...';
