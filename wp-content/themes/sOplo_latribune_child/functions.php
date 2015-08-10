9b72f2c92041351a2ad4c12feb4e316bf01d7ec2
xicaborrero <xicadepato@gmail.com>
2015-08-10 11:32:03 -0500

    Cambios en pagina de accueil
    CSS, e imagenes

--- wp-content/themes/sOplo_latribune_child/functions.php
+++ wp-content/themes/sOplo_latribune_child/functions.php
@@ -7,3 +7,4 @@ wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css'  
 wp_enqueue_style( 'childstyle' );
 }
 add_action( 'wp_enqueue_scripts', 'wp_schools_enqueue_scripts', 11);
+
